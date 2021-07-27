d8fae454a5f79ea546a96cecf54e892232bc31bf
junginho0901 <cn5114555@gmail.com>
2021-07-27 18:15:09 +0900

    new c files

---
+++ c programming practice/c programming practice/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c programming practice
+//
+//  Created by 정인호 on 2021/07/27.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
