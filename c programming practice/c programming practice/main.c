2c92536ac14842a12353895ac0f894cb6b45febd
junginho0901 <cn5114555@gmail.com>
2021-07-28 16:04:05 +0900

    20210728update8

--- c programming practice/c programming practice/main.c
+++ c programming practice/c programming practice/main.c
@@ -13,7 +13,7 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 */
-
+/*
 #include<stdio.h>
 int main(void)
 {
@@ -32,3 +32,52 @@ int main(void)
     
     printf("합은 : %d , 평균은 : %d",first_num+second_num+third_num,(first_num+second_num+third_num)/3);
 }
+*/
+/* //예제 03 168.p
+#include<stdio.h>
+int main(void)
+{
+    int x=10,y=20,z=0;
+    printf("x=%d y=%d\n",x,y);
+    z=x;
+    x=y;
+    y=x;
+    printf("x=%d y=%d\n",x,y);
+    return 0;
+}
+*/
+
+/*
+#include<stdio.h>
+int main(void)
+{
+    int x;
+    char ch;
+    printf("아스키 코드값을 입력하세요 =");
+    scanf("%d",&x);
+    ch=x;
+    printf("문자 : %c입니다.",x);
+    return 0;
+    
+}
+*/
+/*
+#include<stdio.h>
+int main(void)
+{
+    printf("\a 화재가 발생하였습니다.");
+    return 0;
+}
+*/
+/*
+#include<stdio.h>
+int main(void)
+{
+    char cd = 'a';
+    int b = cd;
+    printf("%d\n",cd);
+    printf("%c, %c, %c",b,b+1,b+2);
+    return 0;
+}
+*/
+
