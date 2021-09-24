eeadda7c610d69aca285320bf7e74c999994689b
junginho0901 <cn5114555@gmail.com>
2021-09-24 17:42:45 +0900

    20210924(3)

--- WebDevCurriculum/Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ WebDevCurriculum/Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -2,15 +2,18 @@ class Desktop {
     // 프로그램을 실행시켰을때 바탕화면의 기초 세팅을 하기위해서
     #dom
     #icon
-	#Forder
-    constructor(icon,forder) {
-        this.#rendering(icon,forder);
-		this.forder=forder;
+	#Folder
+    constructor(icon,folder) {
+        this.#rendering(icon,folder);
+		this.folder=folder;
 		this.icon=icon;
-    }
+    }//아이콘 폴더의 개수를 생성자로 받아서 rendering에 넣고 실행.
     // 화면에 출력해 주는 것
-    #rendering(icons,forders) {
+    #rendering(icons,folders) {
         // 렌더링 로직
+		//각각 아이콘과 폴더의 개수를 받은만큼 객체를 만들어 냄
+		this.#icon=new Icon();
+		this.#Folder=new Folder();
     }
 }
 
@@ -50,8 +53,12 @@ class Window {
     #dom
 	constructor(){
 		this.Drag();
+		this.CloseWindow;
 	}
 	Drag(){
+
+	}
+	CloseWindow(){
 
 	}
 }
