c95d25d978c2b290f6447ea4f2c8395f3f9181ee
junginho0901 <cn5114555@gmail.com>
2021-09-24 13:23:59 +0900

    20210924(1)

--- WebDevCurriculum/Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
+++ WebDevCurriculum/Part 2. JavaScript/Quest 06. JavaScript OOP - Design/skeleton/desktop.js
@@ -1,19 +1,6 @@
 class Desktop {
 	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-	constructor(Ico,Folde,Windo){
-		this.Ico=Ico;
-		this.Folde=Folde;
-		this.Windo=Windo;
-	}
-	ShowWindow(){
-	Folde.ondblclick=function(){Windo.style.display='block';};
-	}
 };
-let Ic=document.getElementById('Icon1');
-let Fold=document.getElementById('Folder1');
-let Wind=document.getElementById('Window1');
-let Deskt=new Desktop(Ic,Fold,Wind);
-Deskt.ShowWindow();
 
 class Icon {
 	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
