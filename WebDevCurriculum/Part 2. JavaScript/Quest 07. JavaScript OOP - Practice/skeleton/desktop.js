1a53e4cedbfbffe5bb8d562b4a214e247ebb688d
junginho0901 <cn5114555@gmail.com>
2021-09-30 16:01:03 +0900

    20210930 js desktop code 추가

--- WebDevCurriculum/Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
+++ WebDevCurriculum/Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
@@ -1,6 +1,45 @@
 class Desktop {
 	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
-};
+	#dom = document.getElementsByClassName(desktop);
+	#Icon
+	#Folder
+
+	constructor(dsktops,folders,icons){
+		this.#Makedsktop(dsktops)
+		this.#rendering(folders,icons)
+	}
+	#Makedsktop(num) {
+		if(num>2)
+		{
+			alert('데스크탑이 너무 많습니다.');
+		}
+		else if (num<=0) 
+		{
+			alert('데스크탑 개수가 올바르지 않습니다.')
+		}
+		else if(num==1)
+		{
+			//화면 100% 데스크탑 1개 생성
+			this.#dom
+		}
+		else if(num==2)
+		{
+			//화면을 브라우저 5:5로 맞추고 데스크탑 2개 생성
+		}
+	}
+	#rendering(fold,icn)
+	{
+		for(i=0;i<fold;i++)
+		{
+			//설정한 폴더 개수만큼 객체 생성
+		}
+		for(i=0;i<icn;i++)
+		{
+			//설정한 아이콘 개수만큼 객체 생성
+		}
+	}
+
+}
 
 class Icon {
 	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
