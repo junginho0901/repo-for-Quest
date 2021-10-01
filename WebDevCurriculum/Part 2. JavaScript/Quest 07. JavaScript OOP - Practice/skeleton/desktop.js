811070d360cc2eafce93c18828f7aad18850bbfa
junginho0901 <cn5114555@gmail.com>
2021-10-01 17:49:07 +0900

    20211001 Makedesktop 함수 변경

--- WebDevCurriculum/Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
+++ WebDevCurriculum/Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
@@ -1,42 +1,50 @@
-function Makedsktop(num) {
-	if(num>2)
-	{
-		alert('데스크탑이 너무 많습니다.');
-	}
-	else if (num<=0) 
-	{
-		alert('데스크탑 개수가 올바르지 않습니다.')
-	}
-	else if(num==1)
-	{
-		//화면 100% 데스크탑 1개 생성
-		this.#dom
-	}
-	else if(num==2)
-	{
-		//화면을 브라우저 5:5로 맞추고 데스크탑 2개 생성
-	}
-}
+
 class Desktop {
 	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
 	#dom
 	#Icon
 	#Folder
-
+	desktops
 	constructor(folders,icons){
-		this.#rendering(folders,icons)
+		this.desktops=prompt("데스크탑의 개수를 입력해주세요.");
+		this.Makedsktop(this.desktops);
+		this.#rendering(folders,icons);
 	}
 	
 	#rendering(fold,icn)
 	{
+		
 		for(i=0;i<fold;i++)
 		{
 			//설정한 폴더 개수만큼 객체 생성
+
 		}
 		for(i=0;i<icn;i++)
 		{
 			//설정한 아이콘 개수만큼 객체 생성
 		}
+	}
+	Makedsktop(num) {
+		if(num>2)
+		{
+			alert('데스크탑이 너무 많습니다.');
+		}
+		else if (num<=0) 
+		{
+			alert('데스크탑 개수가 올바르지 않습니다.')
+		}
+		else
+		{
+			for(i=0;i<num;i++)
+			{
+			//데스크탑 생성
+			}
+			if(num===2)
+			{
+				//화면을 브라우저 5:5로 맞추고 데스크탑 2개 생성 css 변경
+			}
+		}
+		
 	}
 
 }
