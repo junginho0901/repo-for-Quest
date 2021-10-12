bd284ed9077a0eee416492e2a0b6214435daeaa6
Song-YunMin <thd930308@naver.com>
2021-10-12 13:49:25 +0900

    [UPDATE] 윤민 코드

--- Quest(plus)/changingbutton.js
+++ Quest(plus)/changingbutton.js
@@ -1,21 +1,15 @@
-
-function changeValue(button,body){
-    if(button.value=='시작')
-    {button.value="종료";
-    body.style.backgroundColor="gray";}
-    else
-    {button.value="시작";
-    body.style.backgroundColor="white";}
-
+function main () {
+    const btn = document.getElementById('changingButton');
+    const backGroundColor = document.getElementById('bodyClass');
+    btn.addEventListener('click', () => {
+        if(btn.value === '시작'){
+            btn.value = '종료';
+            backGroundColor.style.backgroundColor = 'gray';
+        } else {
+            btn.value = '시작';
+            backGroundColor.style.backgroundColor = 'white';
+        }
+    })
 }
 
-window.onload=function()
-{const btn= document.getElementById('changingButton');
-const bgclr=document.getElementById('bodyClass');
-btn.onclick=function()
-{if(btn.value=='시작')
-{btn.value="종료"; bgclr.style.backgroundColor="gray";}
-else
-{btn.value="시작";
-bgclr.style.backgroundColor="white";}
-}}
+main();
