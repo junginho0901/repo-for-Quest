65c6d1f0071c0c3679d9e3ab1b5ce4684a16a6a0
Song-YunMin <thd930308@naver.com>
2021-10-16 22:07:38 +0900

    [UPDATE] Add Code

--- Quest(plus)/Quest(2)/Car.js
+++ Quest(plus)/Quest(2)/Car.js
@@ -1,30 +1,34 @@
 
-class Car{
-    constructor(){
-        this.MakeButton();
-        this.DeleteButton();
-        this.StartingButton();
-        this.OffButton();
+class Car {
+    #body
+    #startButton
+    constructor(BodyId) {
+        this.#body = BodyId;
+        this.#startButton = BodyId.querySelector('.startButton');
+        this.#bindButtonEvents()
     }
-    startButton=document.getElementById('startButton');
-    offButton=document.getElementById('offButton');
-    StartingButton(){
-        startButton.addEventListener('click',function(){this.MakeButton(this.offButton);})
-        startButton.addEventListener('click',function(){this.DeleteButton(this.startButton);})
-    }
-    OffButton(){
-        offButton.addEventListener('click',function(){this.MakeButton(this.startButton);})
-        offButton.addEventListener('click',function(){this.DeleteButton(this.offButton);})
-    }
-    MakeButton(Button){
-        if(Button.style.visibility==='hidden'){
-        Button.style.visibility='visible';}
-    }
-    DeleteButton(Button){
-        if(Button.style.visibility==='visible'){
-        Button.style.visibility='hidden';}
-    }
-  
-}
-inhoCar = new Car();
 
+    #bindButtonEvents() {
+        this.#startButton.addEventListener('click', () => {
+            if (this.#startButton.value === '시동 끄기'){
+                this.#startButton.value = '시동 켜기';
+                this.#closeFunction();
+            } else {
+                this.#startButton.value = '시동 끄기';
+                this.#prepareFunction();
+            }
+        })
+    }
+
+    #prepareFunction() {
+        this.#body.querySelector('.gasPedal').style.visibility = 'visible';
+        this.#body.querySelector('.break').style.visibility = 'visible'
+        this.#body.querySelector('.oiling').style.visibility = 'visible'
+    }
+
+    #closeFunction() {
+        this.#body.querySelector('.gasPedal').style.visibility = 'hidden';
+        this.#body.querySelector('.break').style.visibility = 'hidden'
+        this.#body.querySelector('.oiling').style.visibility = 'hidden'
+    }
+}
