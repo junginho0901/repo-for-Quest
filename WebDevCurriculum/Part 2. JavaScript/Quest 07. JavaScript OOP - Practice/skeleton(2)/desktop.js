3b65197a2dc7cf7790ea4a46de3140010cff904b
junginho0901 <cn5114555@gmail.com>
2021-10-26 15:30:43 +0900

    데스크탑 아이콘,폴더 개수만큼  뿌리는 클래스

--- WebDevCurriculum/Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton(2)/desktop.js
+++ WebDevCurriculum/Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton(2)/desktop.js
@@ -1,17 +1,33 @@
 class desktop{
     #dom
     #icon
-    constructor(dom){
+    #folder
+    constructor(dom,Icons,Folders){
         this.#dom=dom
-        this.#icon=new Icon();
-        this.makeIcon();
+        this.#icon=[]
+        this.#folder=[]
+        for(let i=0;i<Icons;i++)
+        {
+            this.#icon[i]=new Icon();
+            this.makeIcon(this.#icon[i]);
+        }
+        for(let i=0;i<Folders;i++)
+        {
+            this.#folder[i]=new Folder();
+            this.makeFolder(this.#folder[i]);
+        }
+        
     }
 
-    makeIcon(){
-        this.#dom.appendChild(this.#icon.getIconDom());
+    makeIcon(icon){
+        this.#dom.appendChild(icon.getIconDom());
     }
-    
+    makeFolder(folder){
+        this.#dom.appendChild(folder.getFolderDom());
+    }
 }
+
+
 class Icon{
     #dom
     constructor(){
@@ -22,6 +38,20 @@ class Icon{
     }
     prepareIcon(){
         const t = document.querySelector('.template-icon');     // template DOM Select
+        const tmpl = document.importNode(t.content, true);         // template 활성화 및 포함
+        this.#dom = tmpl.querySelector('.icon');
+    }
+}
+class Folder{
+    #dom
+    constructor(){
+        this.prepareFolder();
+    }
+    getFolderDom(){
+        return this.#dom;
+    }
+    prepareFolder(){
+        const t = document.querySelector('.template-folder');     // template DOM Select
         const tmpl = document.importNode(t.content, true);         // template 활성화 및 포함
         this.#dom = tmpl.querySelector('.icon');
     }
