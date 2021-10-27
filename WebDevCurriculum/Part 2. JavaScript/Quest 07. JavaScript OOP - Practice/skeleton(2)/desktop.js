9d1ddd729141d9c9b0530815733275bce3865a74
junginho0901 <cn5114555@gmail.com>
2021-10-27 17:12:31 +0900

    아이콘 크기 변경(전체 제어 됨. 수정 필요)

--- WebDevCurriculum/Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton(2)/desktop.js
+++ WebDevCurriculum/Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton(2)/desktop.js
@@ -1,103 +1,157 @@
-class desktop{
+class desktop {
     #dom
     icon
     folder
-    
-    constructor(dom,Icons,Folders){
-        this.#dom=dom
-        this.icon=[]
-        this.folder=[]
-        for(let i=0;i<Icons;i++)
-        {
-            this.icon[i]=new Icon();
+    btn
+    imgbutton
+    sizebutton
+    constructor(dom, Icons, Folders) {
+        this.#dom = dom
+        this.icon = []
+        this.folder = []
+        for (let i = 0; i < Icons; i++) {
+            this.icon[i] = new Icon();
             this.makeIcon(this.icon[i]);
         }
-        for(let i=0;i<Folders;i++)
-        {
-            this.folder[i]=new Folder();
+        for (let i = 0; i < Folders; i++) {
+            this.folder[i] = new Folder();
             this.makeFolder(this.folder[i]);
         }
-
+        this.makeButtons();
+        this.changeICon();
     }
 
-    makeIcon(icon){
+    makeIcon(icon) {
         this.#dom.appendChild(icon.getIconDom());
     }
-    makeFolder(folder){
+    makeFolder(folder) {
         this.#dom.appendChild(folder.getFolderDom());
     }
-    
-    
+    makeButtons(button) {
+        this.btn = new Buttons()
+        this.sizebutton = this.btn.getSizeButton();
+        this.imgbutton = this.btn.getImgButton();
+        this.#dom.appendChild(this.sizebutton);
+        this.#dom.appendChild(this.imgbutton);
+    }
+
+    changeICon() {
+        const a = new changeIcon();
+        let width
+        let height
+        this.sizebutton.addEventListener('click', () => {
+            width = prompt('가로px을 입력하세요.예시)50px이라면 50');
+            height = prompt('세로px을 입력하세요.예시)50px이라면 50');
+            if (isNaN(Number(width)) == true && isNaN(Number(height)) == true) { alert("다시 입력해주십시오.") }
+            else { a.changeSize(width, height) }
+        }
+        )
+    }
 }
 
 
-class Icon{
+class Icon {
     #dom
-    constructor(){
+    constructor() {
         this.prepareIcon();
     }
-    getIconDom(){
+    getIconDom() {
         return this.#dom;
     }
-    prepareIcon(){
-        const t = document.querySelector('.template-icon');    
-        const tmpl = document.importNode(t.content, true);    
+    prepareIcon() {
+        const t = document.querySelector('.template-icon');
+        const tmpl = document.importNode(t.content, true);
         this.#dom = tmpl.querySelector('.icon');
     }
 }
-class Folder{
+class Folder {
     #dom
     window
-   
-    constructor(){
-       
+
+    constructor() {
+
         this.prepareFolder();
-        
-        this.window=new Window();
+
+        this.window = new Window();
         this.openWindow();
     }
-    getFolderDom(){
+    getFolderDom() {
         return this.#dom;
     }
-    prepareFolder(){
-        const t = document.querySelector('.template-folder');     
-        const tmpl = document.importNode(t.content, true);        
+    prepareFolder() {
+        const t = document.querySelector('.template-folder');
+        const tmpl = document.importNode(t.content, true);
         this.#dom = tmpl.querySelector('.icon');
     }
-    openWindow(){
-        this.#dom.addEventListener('dblclick',()=>{
+    openWindow() {
+        this.#dom.addEventListener('dblclick', () => {
             this.#dom.after(this.window.getWindowDom());
         })
     }
 }
 
-class Window{
+class Window {
     #dom
-    constructor()
-    {
+    constructor() {
         this.preapareWindow();
         this.clickXbox();
     }
-    getWindowDom(){
+    getWindowDom() {
         return this.#dom;
     }
-    preapareWindow(){
-        const t = document.querySelector('.template-window');    
-        const tmpl = document.importNode(t.content, true);        
+    preapareWindow() {
+        const t = document.querySelector('.template-window');
+        const tmpl = document.importNode(t.content, true);
         this.#dom = tmpl.querySelector('.window');
     }
-    clickXbox(){
-        const x=this.#dom.querySelector('.xbox');
-        x.addEventListener('click',()=>{this.#dom.remove()})
+    clickXbox() {
+        const x = this.#dom.querySelector('.xbox');
+        x.addEventListener('click', () => { this.#dom.remove() })
     }
 }
 
-class Drag{
+class Drag {
     #dom
-    constructor(dom){
-        this.#dom=dom
+    constructor(dom) {
+        this.#dom = dom
 
     }
-    drag(){
+    drag() {
     }
+
+
+}
+
+class Buttons {
+    #sizeButtonDom
+    #imgButtonDom
+    constructor() {
+        this.prepareDom();
+    }
+    prepareDom() {
+        const t = document.querySelector('.template-ChangeSize');
+        const tmpl = document.importNode(t.content, true);
+        this.#sizeButtonDom = tmpl.querySelector('.changeBT');
+        this.#imgButtonDom = tmpl.querySelector('.changeIcon');
+    }
+    getSizeButton() {
+        return this.#sizeButtonDom
+    }
+    getImgButton() {
+        return this.#imgButtonDom
+    }
+}
+
+class changeIcon {
+
+    constructor() {
+
+    }
+    changeSize(Width, Height) {
+        for (let i = 0; i < document.querySelectorAll('.icon').length; i++) {
+            document.querySelectorAll('.icon')[i].style.width = Width + 'px';
+            document.querySelectorAll('.icon')[i].style.height = Height + 'px';
+        }
+    }
+
 }
