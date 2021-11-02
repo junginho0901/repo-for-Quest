854c03f0f4436a164a721d291ecf0d1a61d4e022
junginho0901 <cn5114555@gmail.com>
2021-11-02 15:21:14 +0900

    드래그 기능(현재 위치가 확인이 안됨(아이콘))

--- WebDevCurriculum/Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
+++ WebDevCurriculum/Part 2. JavaScript/Quest 07. JavaScript OOP - Practice/skeleton/desktop.js
@@ -1,8 +1,60 @@
 class Drag{
     #dom
-    constructor(dom){
+    #select
+    constructor(dom)
+    {
         this.#dom=dom;
-
+        this.addDragAndDrop();
     }
+    addDragAndDrop() {
+        // window Drag || Icon Drag
+            this.#select = this.#dom;
+
+        const mouseDownEvent = this.#dom.addEventListener('mousedown', e => {
+            console.log("마우스 내려감")
+            let pushed = true;
+            let mouseCoord = {
+                x: e.clientX,
+                y: e.clientY
+            };
+            console.log(this.#dom.style.left)
+            console.log(this.#dom.style.top)
+            const mouseMoveEvent = document.addEventListener('mousemove', e => {
+                if (pushed) {
+                  
+                    const currCoord = {
+                        x: Number(this.#dom.style.left.replace('px', '')),
+                        y: Number(this.#dom.style.top.replace('px', ''))
+                    };
+                   
+                   
+                    const destCoord = {
+                        x: currCoord.x + e.clientX - mouseCoord.x,
+                        y: currCoord.y + e.clientY - mouseCoord.y
+                    };
+                    this.#dom.style.left = `${destCoord.x}px`;
+                    this.#dom.style.top = `${destCoord.y}px`;
+
+                  
+                    mouseCoord = {
+                        x: e.clientX,
+                        y: e.clientY
+                    };
+                 console.log( this.#dom.style.left )
+                 console.log( this.#dom.style.top)
+                }
+            });
+
+            const mouseUpEvent = document.addEventListener('mouseup', () => {
+               
+                pushed = false;
+                this.#dom.removeEventListener('mousemove', mouseMoveEvent);
+                this.#dom.removeEventListener('mousedown', mouseDownEvent);
+                this.#dom.removeEventListener('mouseup', mouseUpEvent);
+            });
+        });
+    }
+
     
+
 }
